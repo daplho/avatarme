e98334b306272ecfb1bbdaeb2fac48bf3ac13fbc
daplho <nskirov@gmail.com>
2016-11-17 20:46:28 -0800

    added flag support for email and ip address

--- main.go
+++ main.go
@@ -8,11 +8,32 @@ import (
 )
 
 func main() {
+	var email, ip string
+
 	app := cli.NewApp()
-	app.Name = "cool avatar"
-	app.Usage = "blow them out of the water"
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "email",
+			Value:       "avatar@me.com",
+			Usage:       "e-mail address",
+			Destination: &email,
+		},
+		cli.StringFlag{
+			Name:        "ip-address, ip",
+			Value:       "127.0.0.1",
+			Usage:       "ip address",
+			Destination: &ip,
+		},
+	}
+
 	app.Action = func(c *cli.Context) error {
-		fmt.Println("boom!")
+		email := c.String("email")
+		fmt.Printf("received address: %s\n", email)
+
+		ip := c.String("ip-address")
+		fmt.Printf("received ip address: %s\n", ip)
+
 		return nil
 	}
 
